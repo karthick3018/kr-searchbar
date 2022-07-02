5d949ca3d57d81a46a1d889c5dd1905ee8ddd1eb
Karthick <karthickbanfico@Karthicks-MacBook-Pro.local>
2022-07-02 20:01:35 +0530

    initial working repo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,9 @@
 import * as React from "react";
 import "./../assets/scss/App.scss";
 
-const reactLogo = require("./../assets/img/react_logo.svg");
-
 const App = () => (
   <div className="app">
-    <h1>Hello World!</h1>
-    <p>Foo to the barz</p>
-    <img src={reactLogo.default} height="480" />
+    <h1>Search</h1>
   </div>
 );
 
